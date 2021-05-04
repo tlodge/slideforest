8ecca49685be7766e76241790444d480e3f63d2e
Tom Lodge <tlodge@gmail.com>
2021-05-04 19:00:44 +0100

    cp commit

--- pages/index.js
+++ pages/index.js
@@ -31,15 +31,14 @@ const _flatten = list => list.reduce(
 const _slink = (sx, sy, tx, ty) =>{
   return <line key={`${tx}${ty}`} x1={sx} y1={sy} x2={tx} y2={ty} style={{stroke:"#000", strokeWidth:"2"}}></line>       
 }
+
 const _clink = (sx, sy, tx, ty) => {
-  return <path d={`M ${sx},${sy} C ${(sx + tx) / 2},${sy} ${(sx + tx) / 2},${ty} ${tx},${ty}`}/>;
+  return <path d={`M ${sx} ${sy} C ${(sx + tx) / 2} ${sy}, ${(sx + tx) / 2} ${ty}, ${tx} ${ty}`} style={{stroke:"#000", strokeWidth:"2.5", fill:"none"}}></path> 
 }
-
-
 
 const insert = (lookup, slide)=>{
   const children = lookup[slide] || [];
-  return {slide, children : children.map(c => insert(lookup, c))}
+  return {slide, name: slide.split(".")[0], children : children.map(c => insert(lookup, c))}
 }
 
 const convertToHierarchy = (lut)=>{
@@ -95,37 +94,38 @@ export default function Home() {
   const [sh, setSlideHeight] = useState(SLIDEWIDTH*WHRATIO)
   const [translate, setTranslate]= useState(0);
   const [child, setChild] = useState();
-  const [count, setCount]= useState(0);
+  
 
   useEffect(()=>{
     setSlideHeight(sw*WHRATIO);
   }, [sw]);
 
   const slidetree = useD3(
+   
     (svg) => {
+      console.log("AM IN USE D3");
       const dgbox = svg.select("g#dragbox");
-     
-      svg.call(d3z.zoom().on("zoom",  (e)=>{dgbox.attr("transform", e.transform)})).call(d3z.zoom, d3z.zoomIdentity.scale(0.8))
+    
+      svg.call(d3z.zoom().on("zoom",  (e)=>{
+        dgbox.attr("transform", e.transform)
+      }))
      
     } 
-  ,[count]);
+  ,[translate]);
 
   useEffect(()=>{
-
     const _tree = d3h.tree().nodeSize([sw+XPADDING,sh+YPADDING])(d3h.hierarchy(convertToHierarchy(lookuptable), d=>d.children))
     
     //get dimensions of tree for rendering!
     const leaves = _tree.leaves();
-    console.log(leaves);
+   
     const minmax = leaves.reduce((acc, node)=>{
           return {minx:Math.min(acc.minx, node.x), maxx:Math.max(acc.maxx, node.x),miny:Math.min(acc.miny, node.y), maxy:Math.max(acc.maxy, node.y)}
     }, {minx:0, maxx:0, miny:0, maxy:0});
 
-    console.log(minmax);
     const _translate = Math.abs(minmax.minx);
     setDims({w: (minmax.maxx-minmax.minx)+sw+XPADDING, h:(minmax.maxy-minmax.miny)+sh+YPADDING});
     setTranslate(_translate);
-    console.log(minmax, _translate);
     setTree(_tree);
     setChild();
   }, [lookuptable])
@@ -146,10 +146,10 @@ export default function Home() {
       {    
         from : {
           name:node.data.slide,
-          x: node.x,
+          x: node.x + translate,
           y: node.y + sh
         },
-        to : (node.children||[]).map(c=>({slide:c.data.slide,x:c.x, y:c.y}))
+        to : (node.children||[]).map(c=>({slide:c.data.slide,x:c.x+translate, y:c.y}))
       },
       ...(node.children || []).map(c=>links(c))
     ])
@@ -266,12 +266,14 @@ export default function Home() {
     const id = `n${node.x}${node.y}`;
 
     return <g key={id}> 
-                <g key={id} transform={`translate(${node.x}, ${node.y})`}  id="Artboard11">
+     
+                <g key={id} id="slide" transform={`translate(${node.x+translate}, ${node.y+20})`}>
+                 
                   <defs>
                       <image id={`_Image1${id}`} width={`${sw}px`} height={`${sh}px`} xlinkHref={`${path}/${node.data.slide}`}/>
                   </defs>
-                
-                  <use onClick={()=>{nodeSelected(node.data.slide)}} id="pg_0001.png" xlinkHref={`#_Image1${id}`} x="0" y="0" width={`${sw}px`} height={`${sh}px`}/>
+                  <rect id={`${node.data.name}`} x={0} y={0} width={SLIDEWIDTH+12} height={(SLIDEWIDTH*WHRATIO)+12} style={{fill:"rgb(59,59,59)",stroke:"white",strokeWidth:"1.87px"}}/>
+                  <use onClick={()=>{nodeSelected(node.data.slide)}} id="pg_0001.png" xlinkHref={`#_Image1${id}`} x="6" y="6" width={`${sw}px`} height={`${sh}px`}/>
                  
                 </g>)
                 {(node.children || []).map(n=>renderTree(n))}
@@ -289,19 +291,71 @@ export default function Home() {
     const hasparent = node.parent != null;
 
     const renderFromTargets = ()=>{
-      return  (<g>
-                <circle id="acircle" cx={sw} cy={sh} r="8" style={{fill:"#fff",stroke:"#ae2b4d",strokeWidth:"2.5px"}}/>
-                <circle cx={sw} cy={sh} r="3" style={{fill:"#ae2b4d",stroke:"#cc6767",strokeWidth:"2.5px"}}/></g>)
+      return  (<g id={`from${node.data.name}`}>
+                <circle id="bigtarget" cx={sw} cy={sh+28} r="8" style={{fill:"#fff",stroke:"#ae2b4d",strokeWidth:"2.5px"}}/>
+                <circle id="smalltarget" cx={sw} cy={sh+28} r="3" style={{fill:"#ae2b4d",stroke:"#cc6767",strokeWidth:"2.5px"}}/>  
+              </g>)
     }
 
     const renderToTargets = ()=>{
       return  (<g>
+                  {renderControlBar()}
                   <circle cx={sw} cy="0" r="8" style={{fill:"#fff",stroke:"#762bae",strokeWidth:"2.5px"}}/>
-                  <circle cx={sw} cy="0" r="3" style={{fill:"#ae2b4d",stroke:"#6F67CC",strokeWidth:"2.5px"}}/></g>)
+                  <circle cx={sw} cy="0" r="3" style={{fill:"#ae2b4d",stroke:"#6F67CC",strokeWidth:"2.5px"}}/>
+              </g>)
     }
 
+    const renderControlBar = ()=>{
+
+          const rectgroup = [0,1,2].map(i=><rect x={sw-36+(2*i)} y={-5+(2*i)} width={8} height={6} style={{strokeWidth:1, fill:"white", stroke:"black"}}/>);
+          let selected = [];
+
+          const selectsubtree = (e, node)=>{
+            
+            d3.selectAll("g#slide").selectAll("rect").style("fill", "white");
+            d3.selectAll("circle#bigtarget").attr("r", 8);
+            d3.selectAll("circle#smalltarget").attr("r", 3);
+
+            if (selected.length > 0){
+              selected = [];
+              return;  
+            }
+            node.each(n=>{
+              selected = [...selected, n];
+              d3.select(`rect#${n.data.name}`).style("fill", "#CC6767");
+            });
+
+            const parentfor = (n={})=>{
+              const parent = n.parent;
+
+              if (!parent)
+                return;
+              
+              d3.select(`g#from${parent.data.name}`).selectAll("circle#bigtarget").attr("r", 12);
+              d3.select(`g#from${parent.data.name}`).selectAll("circle#smalltarget").attr("r", 8);
+
+              parentfor(parent);
+            }
+            parentfor(node.parent);
+          }
+          
+          return <g id ={node.data.name}>
+                    
+                    <g onClick={(e)=>selectsubtree(e,node)}> 
+                      <circle  cx={sw-30} cy="0" r="10" style={{fill:"#fff",stroke:"black",strokeWidth:"2.5px"}}/>
+                      {rectgroup}
+                    </g>
+
+                    <circle cx={sw+30} cy="0" r="10" style={{fill:"#fff",stroke:"black",strokeWidth:"2.5px"}}/>
+                    <line x1={sw-20} x2={sw-8} y1={0} y2={0} style={{strokeWidth:2.5, stroke:"black"}}/>
+                    <line x1={sw+20} x2={sw+8} y1={0} y2={0} style={{strokeWidth:2.5, stroke:"black"}}/>
+                   
+                    <rect x={sw+25} y={-4} width={10} height={8} style={{strokeWidth:1, fill:"white", stroke:"black"}}/>;
+                  </g>
+          
+    }
     return <g key={id}> 
-                <g key={id} transform={`translate(${node.x - (sw/2)}, ${node.y})`}  id="Artboard11">
+                <g key={id} transform={`translate(${node.x + translate - (sw/2)}, ${node.y})`}  id="Artboard11">
                 
                   {hasparent && renderToTargets()}
                   {haschildren && renderFromTargets()}
@@ -318,12 +372,13 @@ export default function Home() {
         return link.to.map((l)=>{
 
             return <g key={`${l.x},${l.y}`}>
-                        {_slink(link.from.x+(sw/2), link.from.y, l.x+(sw/2), l.y)}
+                        {_clink(link.from.x+(sw/2), link.from.y+28, l.x+(sw/2), l.y)}
+                        <line x1={l.x+(sw/2)} x2={l.x+(sw/2)} y1={l.y} y2={l.y+20} style={{strokeWidth:2.5, stroke:"black"}}/>
                    </g>
         });
     });
 }
-
+//transform={`translate(${translate},0)`}>
   return (
     <div >
       <Head>
@@ -335,11 +390,13 @@ export default function Home() {
         <button onClick={()=>setCount(count+1)}>click me!</button>
             <UiFileInputButton label="Upload Single File" uploadFileName="thePdf" onChange={onChange}/>
             <svg ref={slidetree} width={`${dims.w}px`} height={`${dims.h}px`}>
-              <g id="dragbox" style={{fill:"red"}} transform={`translate(${translate},0)`}>
+              
+              <g id="dragbox"  style={{fill:"red"}}> 
                 {renderTree(tree)}
                 {renderLinks(links(tree))}
                 {renderTargets(tree)}
                 </g>
+                
             </svg>
       </main>
     </div>
